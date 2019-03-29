6aca475e91035bf5716eb48ac1db5ccb89d666b6
Dilip <dilip_agheda@yahoo.com>
2019-03-29 21:04:09 +1100

    Added HOC for private routes

---
+++ client/src/HOC/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import store from '../common/store';
+import { Redirect } from 'react-router-dom'
+import {
+    Route
+  } from 'react-router-dom';
+
+const isSignedIn = () =>{
+    const state = store.getState();
+    const isSignedIn = state.userAuth.isSignedIn;
+
+    console.log("Private route");
+    console.log(state);
+    return isSignedIn;
+}
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    return   <Route {...rest} render={(props)=>(isSignedIn()?<Component />:<Redirect to={{pathname:"/signin",state: { from: props.location }}} />)} />
+}
+
+export default PrivateRoute
