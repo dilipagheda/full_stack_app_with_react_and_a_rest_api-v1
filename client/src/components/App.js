55ac4fbe143ae2221ddd07dd06b6f06ceb8c797e
Dilip <dilip_agheda@yahoo.com>
2019-03-29 21:01:15 +1100

    Added Switch for /create and /create/:id routes so that /create doesn't match both routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,13 +3,15 @@ import {
   BrowserRouter,
   Route
 } from 'react-router-dom';
+import {Switch} from 'react-router';
 import Courses from './Courses';
 import Header from './Header';
 import UserSignIn from './UserSignIn';
 import UserSignUp from './UserSignUp';
 import UserSignOut from './UserSignOut';
 import CourseDetail from './CourseDetail';
-
+import CreateCourse from './CreateCourse';
+import PrivateRoute from '../HOC/PrivateRoute';
 
 class App extends Component {
   render() {
@@ -18,7 +20,10 @@ class App extends Component {
         <BrowserRouter>
           <Header />
           <Route path="/" exact component={Courses} />
-          <Route path="/courses/:id" component={CourseDetail} />
+          <Switch>
+            <PrivateRoute path="/courses/create" exact component={CreateCourse} />
+            <Route path="/courses/:id"  component={CourseDetail} />
+          </Switch>
           <Route path="/signin" component={UserSignIn} />
           <Route path="/signup" component={UserSignUp} />
           <Route path="/signout" component={UserSignOut} />
