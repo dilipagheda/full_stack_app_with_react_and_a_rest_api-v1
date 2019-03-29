9fbe121e03b8d121674f3787c895bc39f434bd8d
Dilip <dilip_agheda@yahoo.com>
2019-03-29 21:04:38 +1100

    removed unnecessary imports from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './reducers'
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store = createStore(rootReducer,composeWithDevTools());
+import store from './common/store'
 
 ReactDOM.render(
     <Provider store={store}>
