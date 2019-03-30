9d05c70673b5de443d1df366d126d67bed75cba7
Dilip <dilip_agheda@yahoo.com>
2019-03-30 18:59:36 +1100

    refactored ActionBar component into a class based component and implemented handleDelete method

--- client/src/common/ActionBar.js
+++ client/src/common/ActionBar.js
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux'
 
-const renderActionButtons = (user,course,handleDelete) => {
+const axios = require('axios');
 
-    if(user && course && course.user && course.user._id===user._id){
+class ActionBar extends Component {
+
+    handleDelete = ()=>{
+        if(!this.props || !this.props.user ||!this.props.user.token)return;
+        axios({
+          url:`http://localhost:5000/api/courses/${this.props.course._id}`,
+          method:'delete',
+          headers:{
+              Authorization:`Basic ${this.props.user.token}`
+          }
+        })
+        .then( response=> {
+          // handle success
+          this.props.redirectToHome();
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });
+      }
+
+    renderActionButtons = () => {
+        if(this.props.user && this.props.course && this.props.course.user && this.props.course.user._id===this.props.user._id){
+            return (
+                <span>
+                    <Link className="button" to={{pathname:`/courses/${this.props.course._id}/update`,
+                    state:{
+                    course:this.props.course
+                    }}}>Update Course</Link>
+                    <button className="button" onClick={this.handleDelete} >Delete Course</button>
+                </span>
+            );
+        }else{
+            return null;
+        }
+    }
+
+    render(){
         return (
-            <span>
-                <Link className="button" to={{pathname:`/courses/${course._id}/update`,
-                state:{
-                course:course
-                }}}>Update Course</Link>
-                <button className="button" onClick={handleDelete} >Delete Course</button>
-            </span>
+            <div className="actions--bar">
+            <div className="bounds">
+              <div className="grid-100">
+                {this.renderActionButtons()}
+              <Link className="button button-secondary" to="/">Return to List</Link>
+              </div>
+            </div>
+            </div>
         );
-    }else{
-        return null;
     }
 }
 
-const ActionBar = (props) => {
-    return (
-        <div className="actions--bar">
-        <div className="bounds">
-          <div className="grid-100">
-            {renderActionButtons(props.user,props.course,props.handleDelete)}
-          <Link className="button button-secondary" to="/">Return to List</Link>
-          </div>
-        </div>
-        </div>
-    );
-}
-
-export default ActionBar
+const mapStateToProps = state => {
+    return {user:state.userAuth.user}
+  }
+  
+export default connect(mapStateToProps)(ActionBar)
