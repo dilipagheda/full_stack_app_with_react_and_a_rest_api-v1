9d8bfbea847252f346ace8c0ce034c068dfb5a06
Dilip <dilip_agheda@yahoo.com>
2019-03-31 17:53:43 +1100

    Refactored Header to a proper class component with all methods are part of it

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,35 +2,39 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux'
 
-
-const withSignIn = (username) => {
-  return (
-    <div className="bounds">
-    <h1 className="header--logo">Courses</h1>
-    <nav>
-      <span>Welcome {username}!</span>
-      <Link className="signout" to="/signout">Sign Out</Link>
-    </nav>
-  </div>
-  );
-}
-
-const withOutSignIn = () => {
-  return ( 
-      <div className="bounds">
-        <h1 className="header--logo">Courses</h1>
-        <nav>
-          <Link className="signup" to="/signup">Sign Up</Link>
-          <Link className="signin" to="/signin" >Sign In</Link>
-        </nav>
-      </div>
-  );
-}
-
 class  Header extends Component {
+
+  withSignIn = (username) => {
+    return (
+      <div className="bounds">
+      <h1 className="header--logo">Courses</h1>
+      <nav>
+        <span>Welcome {username}!</span>
+        <Link className="signout" to="/signout">Sign Out</Link>
+      </nav>
+    </div>
+    );
+  }
+
+  withOutSignIn = () => {
+    return ( 
+        <div className="bounds">
+          <h1 className="header--logo" onClick={this.handleLogoOnClick}>Courses</h1>
+          <nav>
+            <Link className="signup" to="/signup">Sign Up</Link>
+            <Link className="signin" to="/signin" >Sign In</Link>
+          </nav>
+        </div>
+    );
+  }
+
+  handleLogoOnClick = () =>{
+    this.props.history.push("/");
+  }
+  
   render(){
    return(<div className="header">
-      {this.props.isSignedIn?withSignIn(this.props.userName):withOutSignIn()}
+      {this.props.isSignedIn?this.withSignIn(this.props.userName):this.withOutSignIn()}
     </div>
    );
   }
